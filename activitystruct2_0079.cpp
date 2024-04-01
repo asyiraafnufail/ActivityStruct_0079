bc217bce2cc3c20b61bca84a9cf760d32a1b62b8
asyiraafnufail <asyiraaf4@gmail.com>
2024-04-01 14:17:25 +0700

    menuliskan cin alamat desa

--- activitystruct2_0079.cpp
+++ activitystruct2_0079.cpp
@@ -19,8 +19,10 @@ int main() {
     cin >> mhs.nim;
     cout << "Nama Mahasiswa : ";
     cin >> mhs.nama;
+
     cout << "Alamat Mahasiswa :";
     cout << "\t Nama Desa : ";
+    cin >> mhs.alamat.desa;
     
     cout << "Umur Mahasiswa : ";
     cin >> mhs.umur;
