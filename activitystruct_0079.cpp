4ea4c86f41d57d20aadda2a23cf479981eb191e0
asyiraafnufail <asyiraaf4@gmail.com>
2024-04-01 13:49:36 +0700

    menuliskan cin untuk input alamat mahasiswa

--- activitystruct_0079.cpp
+++ activitystruct_0079.cpp
@@ -14,6 +14,6 @@ int main() {
     cin >> mhs.nim;
     cout << "Nama Mahasiswa : ";
     cin >> mhs.nama;
-    cout >> "Alamat Mahasiswa :";
-    
+    cout << "Alamat Mahasiswa :";
+    cin >> mhs.alamat;
 }
